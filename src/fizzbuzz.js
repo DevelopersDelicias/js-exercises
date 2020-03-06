ddafc5513225847d4da31b999ee96b71696b7ffa
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2020-03-06 11:49:45 -0700

    🚧 Initial solution

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,3 +1,8 @@
-const fizzbuzz = () => {}
+const fizzbuzz = number => {
+  if (number % 15 === 0) return 'FizzBuzz'
+  if (number % 3 === 0) return 'Fizz'
+  if (number % 5 === 0) return 'Buzz'
+  return `${number}`
+}
 
 module.exports = fizzbuzz
