e50e71218ec917eff7ca1fb425633589599b3eb8
Benjamin Cisneros <bcisneros@tiempodevelopment.com>
2020-03-06 21:12:22 -0700

    :fire: Remove empty lines in test

--- tests/fizzbuzz.test.js
+++ tests/fizzbuzz.test.js
@@ -3,7 +3,6 @@ const fizzbuzz = require('../src/fizzbuzz')
 describe('fizzbuzz()', () => {
   describe('returns "Fizz" when number is multiple of 3', () => {
     const numbers = [3, 6, 9, 12]
-
     numbers.forEach(number => {
       it(`for ${number} should return "Fizz"`, () => {
         expect(fizzbuzz(number)).toEqual('Fizz')
@@ -13,7 +12,6 @@ describe('fizzbuzz()', () => {
 
   describe('returns "Buzz" when number is multiple of 5', () => {
     const numbers = [5, 10, 20, 25]
-
     numbers.forEach(number => {
       it(`for ${number} should return "Buzz"`, () => {
         expect(fizzbuzz(number)).toEqual('Buzz')
@@ -23,7 +21,6 @@ describe('fizzbuzz()', () => {
 
   describe('returns "FizzBuzz" when number is multiple of 3 and 5', () => {
     const numbers = [15, 30, 45, 60]
-
     numbers.forEach(number => {
       it(`for ${number} should return "FizzBuzz"`, () => {
         expect(fizzbuzz(number)).toEqual('FizzBuzz')
@@ -33,7 +30,6 @@ describe('fizzbuzz()', () => {
 
   describe('returns given number when number is not multiple of 3 or 5', () => {
     const numbers = [1, 2, 4, 7, 8, 11]
-
     numbers.forEach(number => {
       it(`for ${number} should return "${number}"`, () => {
         expect(fizzbuzz(number)).toEqual(`${number}`)
