fab2143933c89614dd970f9a12805e67ec7c732a
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2021-09-07 09:37:46 -0600

    :sparkles: Implement asRoman() function

--- src/roman_numbers.js
+++ src/roman_numbers.js
@@ -1,2 +1,25 @@
-module.exports.asRoman = () => {}
+const ROMANS = [
+  { decimal: 1000, roman: 'M' },
+  { decimal: 900, roman: 'CM' },
+  { decimal: 500, roman: 'D' },
+  { decimal: 400, roman: 'CD' },
+  { decimal: 100, roman: 'C' },
+  { decimal: 90, roman: 'XC' },
+  { decimal: 50, roman: 'L' },
+  { decimal: 40, roman: 'XL' },
+  { decimal: 10, roman: 'X' },
+  { decimal: 9, roman: 'IX' },
+  { decimal: 5, roman: 'V' },
+  { decimal: 4, roman: 'IV' },
+  { decimal: 1, roman: 'I' },
+]
+module.exports.asRoman = (decimal) => {
+  return ROMANS.reduce((acc, roman) => {
+    while (decimal >= roman.decimal) {
+      acc += roman.roman
+      decimal -= roman.decimal
+    }
+    return acc
+  }, '')
+}
 module.exports.asDecimal = () => {}
