ccc694dccdad750e69d7454b8ebbfd24c7c6a5c2
Benjamin Cisneros Barraza <cisnerosbarraza@gmail.com>
2022-06-07 14:12:40 -0600

    Refactor code

--- tests/product_exclusion.test.js
+++ tests/product_exclusion.test.js
@@ -1,85 +1,23 @@
 const productExclusion = require('../src/product_exclusion')
 
+const validate = ({ input, expected }) => {
+  const result = productExclusion(input)
+
+  expect(result).toEqual(expected)
+}
+
+const randomInteger = () => Math.floor(Math.random() * 100 + 1)
+
 describe('productExclusion()', () => {
-  it('should return [] when input is empty', () => {
-    const input = []
-    const expected = []
-
-    const result = productExclusion(input)
-
-    expect(result).toEqual(expected)
-  })
-
-  it('should return [1] when input has length of 1', () => {
-    const number = Math.floor(Math.random() * 100 + 1)
-    const input = [number]
-    const expected = [1]
-
-    const result = productExclusion(input)
-
-    expect(result).toEqual(expected)
-  })
-
-  it('should return [1, 2] when input is [2, 1]', () => {
-    const input = [2, 1]
-    const expected = [1, 2]
-
-    const result = productExclusion(input)
-
-    expect(result).toEqual(expected)
-  })
-
-  it('should return [2, 1] when input is [1, 2]', () => {
-    const input = [1, 2]
-    const expected = [2, 1]
-
-    const result = productExclusion(input)
-
-    expect(result).toEqual(expected)
-  })
-
-  it('should return [6, 3, 2] when input is [1, 2, 3]', () => {
-    const input = [1, 2, 3]
-    const expected = [6, 3, 2]
-
-    const result = productExclusion(input)
-
-    expect(result).toEqual(expected)
-  })
-
-  it('should return [6, 3, 2] when input is [1, 2, 3, 4]', () => {
-    const input = [1, 2, 3, 4]
-    const expected = [24, 12, 8, 6]
-
-    const result = productExclusion(input)
-
-    expect(result).toEqual(expected)
-  })
-
-  it('should return [60, 40, 30, 24] when input is [2, 3, 4, 5]', () => {
-    const input = [2, 3, 4, 5]
-    const expected = [60, 40, 30, 24]
-
-    const result = productExclusion(input)
-
-    expect(result).toEqual(expected)
-  })
-
-  it('should return [0, 0, 0, 0] when input is [0, 0, 4, 5]', () => {
-    const input = [0, 0, 4, 5]
-    const expected = [0, 0, 0, 0]
-
-    const result = productExclusion(input)
-
-    expect(result).toEqual(expected)
-  })
-
-  it('should return [20, 0, 0] when input is [0, 4, 5]', () => {
-    const input = [0, 4, 5]
-    const expected = [20, 0, 0]
-
-    const result = productExclusion(input)
-
-    expect(result).toEqual(expected)
-  })
+  it.each([
+    { input: [], expected: [] },
+    { input: [randomInteger()], expected: [1] },
+    { input: [2, 1], expected: [1, 2] },
+    { input: [1, 2], expected: [2, 1] },
+    { input: [1, 2, 3], expected: [6, 3, 2] },
+    { input: [1, 2, 3, 4], expected: [24, 12, 8, 6] },
+    { input: [2, 3, 4, 5], expected: [60, 40, 30, 24] },
+    { input: [0, 0, 4, 5], expected: [0, 0, 0, 0] },
+    { input: [0, 4, 5], expected: [20, 0, 0] },
+  ])('$input => $expected', validate)
 })
